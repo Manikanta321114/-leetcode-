3bd1a9460af473c0f6b9cc406764ea455ee7bcba
Manikanta sn <manimanikantasn@gmail.com>
2026-05-15 22:25:22 +0530

    Time: 0 ms (100%), Space: 12.4 MB (53.77%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+
+    def isHappy(self, n):
+
+        seen = set()
+
+        while n != 1 and n not in seen:
+
+            seen.add(n)
+
+            total = 0
+
+            while n > 0:
+
+                digit = n % 10
+                total = total + digit * digit
+
+                n = n // 10
+
+            n = total
+
+        return n == 1
+        
